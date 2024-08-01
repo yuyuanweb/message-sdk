e8fcfb77488dba9fb4fe80a10096052052d888bd
pine <2268143474@qq.com>
2024-08-01 16:09:39 +0800

    chore: 补充 README.md

---
+++ README.md
@@ -0,0 +1,86 @@
+# 消息推送 SDK
+
+## 背景
+在开发中，我们经常会通过各种渠道通知用户，为了复用和简化通知功能的开发量，我们开发了本 sdk。
+
+## 快速开始
+
+### 引入依赖
+> 当前最新版本是 0.0.5
+```xml
+<dependency>
+    <groupId>io.github.yuyuanweb</groupId>
+    <artifactId>message-sdk</artifactId>
+    <version>version</version>
+</dependency>
+```
+### 加入配置
+```yaml
+tencent:
+  # 腾讯短信服务
+  sms:
+    secretId: xxx
+    secretKey: xxx
+    sdkAppId: xxx
+    signName: xxx
+  ses:
+    secretId: xxx
+    secretKey: xxx
+    user: xxx
+    password: xxx
+    nickName: xxx
+notify:
+  workwx:
+    webhook: xxx
+wx:
+  mp:
+    token: xxx
+    appId: xxx
+    aesKey:
+    secret: xxx
+    config-storage:
+      http-client-type: HttpClient
+      key-prefix: xxx
+      redis:
+        host: 127.0.0.1
+        port: 6379
+      type: Memory
+```
+如上图示例配置所示，目前支持 **短信、邮箱、企微、微信服务号** 四种方式，在使用某种方式之前，需要先填入对应的配置
+
+### 测试使用
+```java
+@SpringBootTest
+class MessageSdkApplicationTests {
+
+    @Resource
+    private AggregatedMessageServiceImpl aggregatedMessageServiceImpl;
+
+    @Test
+    void contextLoads() {
+
+        WorkwxMessage workwxMessage = new WorkwxMessage();
+        workwxMessage.setContent("??test哈哈哈😋");
+        aggregatedMessageServiceImpl.send(workwxMessage);
+
+    }
+
+}
+```
+在上述例子中，我们创建了 WorkwxMessage 对象，然后使用 AggregatedMessageServiceImpl 发送出去，这样就成功发送了一条企微的通知消息。
+同样，需要发送其他类型的消息时，只需要创建对应的消息对象，然后使用 AggregatedMessageServiceImpl 的 send 方法就可以了。
+所支持的消息都是 Message 的子类：
+![img.png](image/img.png)
+
+## 扩展
+1）有时候，我们需要把本次发送的消息存到数据库，或者做一些其他的后置操作，这时候只需要继承 AggregatedMessageServiceImpl 并实现其中的 saveMessage 方法就可以实现。
+像这样：
+![img.png](image/img2.png)
+之后仍然可以通过注入 AggregatedMessageServiceImpl 对象的方式使用。
+
+2）AggregatedMessageServiceImpl 只提供了最基础的发送功能，比如对于企微消息，只能发送纯文本，不支持富文本或 markdown 格式。
+有相关需求可以通过在程序中注入对应实现类的方式使用：
+```java
+@Resource
+private WorkwxMessageServiceImpl workwxMessageServiceImpl;
+```
--- src/test/java/com/yuyuan/messagesdk/MessageSdkApplicationTests.java
+++ src/test/java/com/yuyuan/messagesdk/MessageSdkApplicationTests.java
@@ -2,11 +2,6 @@ package com.yuyuan.messagesdk;
 
 import com.yuyuan.messagesdk.model.dto.WorkwxMessage;
 import com.yuyuan.messagesdk.service.AggregatedMessageServiceImpl;
-import me.chanjar.weixin.common.error.WxErrorException;
-import me.chanjar.weixin.mp.api.WxMpService;
-import me.chanjar.weixin.mp.api.WxMpTemplateMsgService;
-import me.chanjar.weixin.mp.bean.template.WxMpTemplateData;
-import me.chanjar.weixin.mp.bean.template.WxMpTemplateMessage;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
@@ -16,55 +11,15 @@ import javax.annotation.Resource;
 class MessageSdkApplicationTests {
 
     @Resource
-    private WxMpService wxMpService;
-
-    @Resource
     private AggregatedMessageServiceImpl aggregatedMessageServiceImpl;
 
     @Test
     void contextLoads() {
 
-        // MpMessage mpMessage = new MpMessage();
-        // mpMessage.setToUser("ovslX6lNcoojWwUGD63vQnGLjiTw");
-        // mpMessage.setTemplateId("14W0bukzpTN-8L4zGgdApMnz7vqXQGZf0xAlpWfSy80");
-        // mpMessage.setUrl("https://www.laoyujianli.com.com");
-        // mpMessage.addData(new WxMpTemplateData("orderId", "123456"));
-        // mpMessage.addData(new WxMpTemplateData("oriStatus", "订单超时"));
-        // mpMessage.addData(new WxMpTemplateData("curStatus", "订单超🤩rfasdf时"));
-        // messageServiceImpl.send(mpMessage);
-        //
-        //
-        // MailMessage mailMessage = new MailMessage();
-        // mailMessage.setEmailAddressSet(Collections.singletonList("18339461129@163.com"));
-        // mailMessage.setSubject("mail test");
-        // mailMessage.setContent("<p>hello, pine</p>");
-        //
-        // messageServiceImpl.send(mailMessage);
-        //
-        // SmsMessage smsMessage = new SmsMessage();
-        // smsMessage.setPhoneNumberSet(Collections.singletonList("18339461129"));
-        // smsMessage.setTemplateId("2180272");
-        // smsMessage.setTemplateParamSet(Collections.singletonList("3451"));
-        //
-        // messageServiceImpl.send(smsMessage);
-
         WorkwxMessage workwxMessage = new WorkwxMessage();
         workwxMessage.setContent("??test哈哈哈😋");
         aggregatedMessageServiceImpl.send(workwxMessage);
 
-    }
-
-    @Test
-    void mpTest() throws WxErrorException {
-        WxMpTemplateMsgService templateMsgService = wxMpService.getTemplateMsgService();
-        WxMpTemplateMessage message = WxMpTemplateMessage.builder()
-                .toUser("ovslX6lNcoojWwUGD63vQnGLjiTw")
-                .templateId("14W0bukzpTN-8L4zGgdApMnz7vqXQGZf0xAlpWfSy80")
-                .build();
-        message.addData(new WxMpTemplateData("orderId", "123456"));
-        message.addData(new WxMpTemplateData("oriStatus", "订单超时"));
-        message.addData(new WxMpTemplateData("curStatus", "订单超🤩rfasdf时"));
-        templateMsgService.sendTemplateMsg(message);
     }
 
 }
